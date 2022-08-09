5da17f752264b436e80359eb9be8b17ec0645f14
Denis Nikolaev <niko.den.vl@gmail.com>
2022-08-09 20:30:26 +0400

    Create game logic

---
+++ Assets/Code/Scripts/GameState.cs
@@ -0,0 +1,204 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class GameState
+{
+    public const int Rows = 8;
+    
+    public const int Columns = 8;
+
+    public Player[,] Board { get; }
+
+    public Dictionary<Player, int> DiscCount { get; }
+
+    public Player CurrentPlayer { get; private set; }
+
+    public bool GameOver { get; private set; }
+
+    public Player Winner { get; private set; }
+
+    public Dictionary<Position, List<Position>> LegalMoves { get; private set; }
+
+    public GameState()
+    {
+        Board = new Player[Rows, Columns];
+        Board[3, 3] = Player.White;
+        Board[3, 4] = Player.Black;
+        Board[4, 3] = Player.Black;
+        Board[4, 4] = Player.White;
+
+        DiscCount = new Dictionary<Player, int>()
+        {
+            { Player.Black, 2 },
+            { Player.White, 2 }
+        };
+
+        CurrentPlayer = Player.Black;
+        LegalMoves = FindLegalMoves(CurrentPlayer);
+    }
+
+    public bool MakeMove(Position position, out MoveInfo moveInfo)
+    {
+        if (!LegalMoves.ContainsKey(position))
+        {
+            moveInfo = null;
+            return false;
+        }
+
+        var movePlayer = CurrentPlayer;
+        var outflanked = LegalMoves[position];
+        Board[position.Row, position.Column] = movePlayer;
+        FlipDiscs(outflanked);
+        UpdateDiscCounts(movePlayer, outflanked.Count);
+        PassTurn();
+
+        moveInfo = new MoveInfo { Player = movePlayer, Position = position, Outflanked = outflanked };
+        return true;
+    }
+
+    public IEnumerable<Position> OccupiedPositions()
+    {
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                if (Board[row, column] != Player.None)
+                {
+                    yield return new Position(row, column);
+                }
+            }
+        }
+    }
+
+    private void FlipDiscs(List<Position> positions)
+    {
+        foreach (var position in positions)
+        {
+            Board[position.Row, position.Column] = Board[position.Row, position.Column].Opponent();
+        }
+    }
+
+    private void UpdateDiscCounts(Player movePlayer, int outflankedCount)
+    {
+        DiscCount[movePlayer] += outflankedCount + 1;
+        DiscCount[movePlayer.Opponent()] -= outflankedCount;
+    }
+
+    private void ChangePlayer()
+    {
+        CurrentPlayer = CurrentPlayer.Opponent();
+        LegalMoves = FindLegalMoves(CurrentPlayer);
+    }
+
+    private Player FindWinner()
+    {
+        if (DiscCount[Player.Black] > DiscCount[Player.White])
+        {
+            return Player.Black;
+        }
+        if (DiscCount[Player.White] > DiscCount[Player.Black])
+        {
+            return Player.White;
+        }
+
+        return Player.None;
+    }
+
+    private void PassTurn()
+    {
+        ChangePlayer();
+
+        if (LegalMoves.Count > 0)
+        {
+            return;
+        }
+
+        ChangePlayer();
+        if (LegalMoves.Count == 0)
+        {
+            CurrentPlayer = Player.None;
+            GameOver = true;
+            Winner = FindWinner();
+        }
+    }
+
+    private bool IsInsideBoard(int rows, int columns)
+    {
+        return rows >= 0 && rows < Rows && columns >= 0 && columns < Columns;
+    }
+
+    private List<Position> OutflankedInDirecion(Position position, Player player, int rowDelta, int columnDelta)
+    {
+        var outflanked = new List<Position>();
+        int row = position.Row + rowDelta;
+        int column = position.Column + columnDelta;
+
+        while (IsInsideBoard(row, column) && Board[row, column] != Player.None)
+        {
+            if (Board[row, column] == player.Opponent())
+            {
+                outflanked.Add(new Position(row, column));
+                row += rowDelta;
+                column += columnDelta;
+            } 
+            else // if (Board[row, column] == player)
+            {
+                return outflanked;
+            }
+        }
+
+        return new List<Position>();
+    }
+
+    private List<Position> Outflanked(Position position, Player player)
+    {
+        var outflanked = new List<Position>();
+
+        for (int rowDelta = -1; rowDelta <= 1; rowDelta++)
+        {
+            for (int columnDelta = -1; columnDelta <= 1; columnDelta++)
+            {
+                if (rowDelta == 0 && columnDelta == 0)
+                {
+                    continue;
+                }
+
+                outflanked.AddRange(OutflankedInDirecion(position, player, rowDelta, columnDelta));
+            }
+        }
+        return outflanked;
+    }
+
+    private bool IsMoveLegal(Player player, Position position, out List<Position> outflanked)
+    {
+        if (Board[position.Row, position.Column] != Player.None)
+        {
+            outflanked = null;
+            return false;
+        }
+
+        outflanked = Outflanked(position, player);
+        return outflanked.Any();
+    }
+
+    private Dictionary<Position, List<Position>> FindLegalMoves(Player player)
+    {
+        var legalMoves = new Dictionary<Position, List<Position>>();
+
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                var position = new Position(row, column);
+
+                if (IsMoveLegal(player, position, out List<Position> outflanked))
+                {
+                    legalMoves[position] = outflanked;
+                }
+            }
+        }
+
+        return legalMoves;
+    }
+}
---
+++ Assets/Code/Scripts/Player.cs
@@ -0,0 +1,19 @@
+public enum Player
+{
+    None,
+    Black,
+    White
+}
+
+public static class PlayerExtensions
+{
+    public static Player Opponent(this Player player)
+    {
+        return player switch
+        {
+            Player.Black => Player.White,
+            Player.White => Player.Black,
+            _ => Player.None
+        };
+    }
+}
